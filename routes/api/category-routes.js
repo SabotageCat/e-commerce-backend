283c9141765348f23893a1f897a8be9e8b91fe3f
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-08-10 16:04:58 -0500

    added delete routes to tag-routes and category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -82,6 +82,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      return res.status(404).json({ message: 'No category found with this id!' });
+    }
+    res.status(200).json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
