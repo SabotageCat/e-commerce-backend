e1fd45fb1e71ca85769aeb8198a96d72c6ed9f1e
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-08-10 14:55:55 -0500

    added delete route to product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -145,6 +145,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      return res.status(404).json({ message: "No product found with this id!"});
+    }
+    res.status(200).json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
